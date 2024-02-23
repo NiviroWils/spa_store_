321604ac01751b94bfda82c42ae78dc458f51085
gr412_vokan <ksu280705@yandex.ru>
2024-02-24 00:18:43 +0700

    Добавлена функция авторизации

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,35 @@
 import { createStore } from 'vuex'
 import axios from "axios";
+import router from "@/router";
 
 export default createStore({
-    
+    state: {
+        fio: '',
+        email: '',
+        password: '',
+        user_token: null,
+        products: [],
+    },
+    mutations: {
+        async login(state) {
+            // eslint-disable-next-line no-unused-vars
+            const data = await axios.post('https://jurapro.bhuser.ru/api-shop/login', {
+                email: state.email,
+                password: state.password
+            })
+                .then(function (response) {
+                    state.user_token = response.data.data.user_token;
+                    localStorage.token = state.user_token;
+                    console.log(response.data.data);
+                })
+                .catch(error => {
+                    console.log(error)
+                    alert('Ошибка. Повторите попытку.');
+                })
+            if (localStorage.token !== undefined && localStorage.token !== null) {
+                router.push('/')
+            }
+        }
+    }
+
 })
---
+++ src/views/LoginView.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Добро пожаловать!</h1>
+  <div class="login-page">
+    <form class="login" @submit.prevent="store.commit('login')">
+      <label>Email</label>
+      <input required v-model="store.state.email" type="email" placeholder="Email" class="form-control">
+      <label>Password</label>
+      <input required v-model="store.state.password" type="password" placeholder="Password" class="form-control">
+      <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+  </div>
+  <router-link to="/">К каталогу</router-link>
+</template>
+
+<script>
+import store from "@/store";
+export default {
+  computed:{
+    store(){
+      return store
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
